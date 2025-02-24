f6c32b5f64ea0fce592915c805c2ac9105cd8697
Deniz Y... <ng.deniz.y@live.de>
2025-02-24 14:57:13 +0100

    Aktualisiere PATH in .bashrc; füge unoptimierte Bildkonfiguration in next.config.ts hinzu.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ const nextConfig = {
     reactStrictMode: true,
     output: "export", // Generiert statische Seiten für "out/"-Ordner
     trailingSlash: true, // Stellt sicher, dass alle URLs mit "/" enden
+
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
